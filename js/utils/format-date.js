1d686787dc70eab7bde6322d5e344b0ab16a74fc
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-11-20 22:58:16 -0600

    minor change

--- js/utils/format-date.js
+++ js/utils/format-date.js
@@ -4,8 +4,11 @@ const DEFAULT_CONFIG = {
     month: 'long',
 };
 
+const capitalizeFirstLetter = (string) =>
+    string.charAt(0).toUpperCase() + string.slice(1);
+
 export const formatDate = (date, config = DEFAULT_CONFIG) =>
-    new Intl.DateTimeFormat('es', config).format(date);
+    capitalizeFirstLetter(new Intl.DateTimeFormat('es', config).format(date));
 
 export const formatTemp = (value) => {
     return `${Math.trunc(value)}°`;

c72551d2e16fa8a4c62529777d9ab7b42382740b
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-11-23 13:46:22 -0600

    Navegación entre tabs

--- js/tabs.js
+++ js/tabs.js
@@ -16,7 +16,21 @@ const week = [
 
 const nextDay = (day) => (day === 6 ? 0 : day + 1);
 
+const handleSelectTabClick = (e) => {
+    const $tabSelected = e.target;
+    const { id } = $tabSelected;
+    const $tabActive = document.querySelector('.tab[aria-selected="true"]');
+    const $tabPanel = document.querySelector(`[aria-labelledby=${id}]`);
+    const $tabPanelPrev = document.querySelector(`.tabPanel:not([hidden])`);
+
+    $tabActive.removeAttribute('aria-selected');
+    $tabSelected.setAttribute('aria-selected', true);
+    $tabPanel.hidden = false;
+    $tabPanelPrev.hidden = true;
+};
+
 $tabList.forEach((tab, index) => {
+    tab.addEventListener('click', handleSelectTabClick);
     if (index === 0) {
         tab.textContent = 'Hoy';
         weekday = nextDay(weekday);
